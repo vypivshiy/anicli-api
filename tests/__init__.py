c31b5bc55850a4875148246beb5d53149610f2d6
Georgiy <{ID}+{username}@users.noreply.github.com>
2022-11-14 13:14:06 +0300

    change mock names

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,2 @@
 from tests.fixtures import fake_extractor
-from tests.fixtures.mock_decoders import mock_kodik, mock_aniboom
+from tests.fixtures.mock_decoders import mock_kodik_transport, mock_aniboom_transport
