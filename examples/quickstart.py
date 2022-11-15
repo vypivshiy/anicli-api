84e53325e3f517f6c9d789d97256d40f56d5ddf0
Georgiy <{ID}+{username}@users.noreply.github.com>
2022-11-15 13:32:23 +0300

    add extra prints

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -4,6 +4,7 @@ from anicli_api.extractors import animego
 def step_by_step_search():
     """simple usage api search example"""
     # init extractor
+    print("RUN STEP BY STEP SEARCH")
     extractor = animego.Extractor()
     # search by string query
     search = extractor.search("lain")
@@ -19,11 +20,12 @@ def step_by_step_search():
 
 def step_by_step_ongoing():
     """simple usage api ongoing example"""
-
+    print("RUN STEP BY STEP ONGOING")
     extractor = animego.Extractor()
     # get all available ongoings
     ongoings = extractor.ongoing()
     ongoing = ongoings[0]
+    print(ongoing.dict())
     anime = ongoing.get_anime()
     episodes = anime.get_episodes()
     videos = episodes[0].get_videos()
@@ -35,12 +37,14 @@ def walk_search():
     """simple usage api walk_search example"""
     extractor = animego.Extractor()
     # method return all raw data
+    print("RUN WALK SEARCH")
     for meta in extractor.walk_search("school"):
         print(meta)
 
 
 def walk_ongoing():
     """simple usage api walk_ongoing example"""
+    print("RUN WALK ONGOING")
     extractor = animego.Extractor()
     # method return all raw data
     # TODO implements check duplicates url
@@ -49,19 +53,24 @@ def walk_ongoing():
 
 
 def example_iter_objects():
+    print("RUN ITER OBJECTS EXAMPLE")
     extractor = animego.Extractor()
     search = extractor.search("lain")[0]
     ongoing = extractor.ongoing()[0]
     # return raw dict all metadata
+    print("SEARCH extract all meta")
+
     for meta in search:
         print(meta)
 
     # return raw dict all metadata
+    print("ONGOING extract all meta")
     for meta in ongoing:
         print(meta)
 
     anime = search.get_anime()
     # iter search objects
+    print("ANIME, EPISODE iter")
     for episode in anime:
         for video in episode:
             print(video.get_source())
