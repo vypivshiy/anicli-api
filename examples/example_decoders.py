26f506390bfdeaf2d9a5fd388b3170d27a9cfb1e
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2022-12-06 19:52:12 +0300

    change comments

--- examples/example_decoders.py
+++ examples/example_decoders.py
@@ -6,13 +6,14 @@ if __name__ == "__main__":
         "https://kodik.info/seria/1026046/02a256101df196484d68d10d28987fbb/720p"
     )
     print(kodik_links)
+    for video in kodik_links:
+        print(video.quality, video.url)
     print("get videos from aniboom")
-    # for playing video from aniboom required extra headers:
-    # user-agent: any desktop or mobile
-    # referer: https://aniboom.one/
-    # accept-language: ru-RU  # increase download speed
-    # example commands:
-    # ffmpeg -user_agent 'Mozilla 5.0' -referer 'https://aniboom.one/' -headers 'accept-language: ru-RU' -i 'URL' out.mp4
-    # mpv 'URL' -http-header-fields "Referer: https://aniboom.one,Accept-Language: ru-RU, User-Agent:Mozilla 5.0"
+
     aniboom_links = Aniboom.parse("https://aniboom.one/embed/N9QdKm4Mwz1?episode=1&translation=2")
-    print(*aniboom_links, sep="\n")
+    for video in aniboom_links:
+        # required headers contains in extra_headers attribute:
+        # example commands for play video or download:
+        # ffmpeg -user_agent 'Mozilla 5.0' -referer 'https://aniboom.one/' -headers 'accept-language: ru-RU' -i 'URL' out.mp4
+        # mpv 'URL' -http-header-fields "Referer: https://aniboom.one,Accept-Language: ru-RU, User-Agent:Mozilla 5.0"
+        print(video.quality, video.url, video.extra_headers)
