8aa82d3ccdf023cab3d7b2429164570cfec7e2ca
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-01-12 17:21:12 +0300

    add yt-dlp decoder (not tested)

--- anicli_api/decoders/__init__.py
+++ anicli_api/decoders/__init__.py
@@ -6,5 +6,7 @@ from anicli_api.decoders.kodik import Kodik
 from anicli_api.decoders.okru import OkRu
 from anicli_api.decoders.sibnet import Sibnet
 from anicli_api.decoders.vkcom import VkCom
+from anicli_api.decoders.yt_dlp_adapter import YtDlpAdapter
+
 
 ALL_DECODERS = [Kodik, Aniboom, Sibnet, CsstOnline, VkCom, Dzen, OkRu, AnimeJoyDecoder]
