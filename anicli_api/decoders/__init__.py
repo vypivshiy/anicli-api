e39bdb41b8631cc11a5bf579d12a95b3e9f9d027
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-01-12 13:18:30 +0300

    add csst import

--- anicli_api/decoders/__init__.py
+++ anicli_api/decoders/__init__.py
@@ -1,5 +1,6 @@
 from anicli_api.decoders.aniboom import Aniboom
+from anicli_api.decoders.csst import CsstOnline
 from anicli_api.decoders.kodik import Kodik
 from anicli_api.decoders.sibnet import Sibnet
 
-ALL_DECODERS = [Kodik, Aniboom, Sibnet]
+ALL_DECODERS = [Kodik, Aniboom, Sibnet, CsstOnline]
