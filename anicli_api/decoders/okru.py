970662dc2d1f3a1b6742e5162b3ccdae86678571
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-01-12 14:49:09 +0300

    add okru decoder

---
+++ anicli_api/decoders/okru.py
@@ -0,0 +1,44 @@
+import re
+from typing import List
+from html import unescape
+
+from anicli_api.base_decoder import BaseDecoder, MetaVideo
+
+
+class OkRu(BaseDecoder):
+    URL_RULE = re.compile(r"ok\.ru/videoembed/\d+")
+
+    @staticmethod
+    def _parse_video(response: str) -> List[MetaVideo]:
+        objects: List[MetaVideo] = []
+        response = unescape(response).replace('\\"', '"')
+        for match in re.finditer(r'"ondemand(?:Hls|Dash)":"(?P<url>https?://.*?)"', response):
+            if match.groupdict()["url"].endswith(".m3u8"):
+                objects.append(MetaVideo(type="m3u8", quality=1080, url=match.groupdict()["url"]))
+            elif match.groupdict()["url"].endswith(".mpd"):
+                objects.append(MetaVideo(type="mpd", quality=1080, url=match.groupdict()["url"]))
+        return objects
+
+    @classmethod
+    def parse(cls, url: str, **kwargs) -> List[MetaVideo]:
+        cls._validate_url(url)
+        cls_ = cls(**kwargs)
+
+        with cls_.http as session:
+            response = session.get(url)
+            return cls_._parse_video(response.text)
+
+    @classmethod
+    async def async_parse(cls, url: str, **kwargs) -> List[MetaVideo]:
+        cls._validate_url(url)
+        cls_ = cls(**kwargs)
+
+        async with cls_.a_http as session:
+            response = await session.get(url)
+            return cls_._parse_video(response.text)
+
+
+
+
+
+
---
+++ tests/decoders/test_okru.py
@@ -0,0 +1,38 @@
+import httpx
+import pytest
+
+from anicli_api.decoders import OkRu
+
+RAW_RESPONSE = ''',"ondemandHls":"https://vd300.mycdn.me/expires/0/clientType/0/srcIp/192.168.0.1/type/2/mid/0/id/1/ms/127.0.0.1/srcAg/CHROME_ANDROID/oq/0/ct/28/sig/foo/ondemand/hls2_123.abc.m3u8","ondemandDash":"https://vd300.mycdn.me/expires/0/clientType/0/srcIp/192.168.0.1/type/2/mid/0/id/1/ms/127.0.0.1/srcAg/CHROME_ANDROID/oq/0/ct/29/sig/foo/ondemand/dash2_123.abc.mpd",'''
+
+
+def mock_transport():
+    def handler(_):
+        return httpx.Response(200, text=RAW_RESPONSE)
+
+    transport = httpx.MockTransport(handler)
+    return transport
+
+
+def test_cmp():
+    assert "https://google.com" != OkRu()
+    assert "https://ok.ru/videoembed/123" == OkRu()
+
+
+def test_parse():
+    result = OkRu.parse("https://ok.ru/videoembed/123", transport=mock_transport())
+    assert result[0].dict() == {'extra_headers': {},
+ 'quality': 1080,
+ 'type': 'm3u8',
+ 'url': 'https://vd300.mycdn.me/expires/0/clientType/0/srcIp/192.168.0.1/type/2/mid/0/id/1/ms/127.0.0.1/srcAg/CHROME_ANDROID/oq/0/ct/28/sig/foo/ondemand/hls2_123.abc.m3u8'}
+
+
+@pytest.mark.asyncio
+async def test_async_parse():
+    result = await OkRu.async_parse(
+        "https://ok.ru/videoembed/123", transport=mock_transport()
+    )
+    assert result[0].dict() == {'extra_headers': {},
+ 'quality': 1080,
+ 'type': 'm3u8',
+ 'url': 'https://vd300.mycdn.me/expires/0/clientType/0/srcIp/192.168.0.1/type/2/mid/0/id/1/ms/127.0.0.1/srcAg/CHROME_ANDROID/oq/0/ct/28/sig/foo/ondemand/hls2_123.abc.m3u8'}
