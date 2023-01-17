7ae4f9c71d0ea99e1f1eeb35f8c666fa047dc357
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-01-17 13:19:46 +0300

    refactoring logger

--- anicli_api/logging_config.py
+++ anicli_api/logging_config.py
@@ -1,12 +1,25 @@
 import logging
 
 FORMAT_LOG = "%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s"
-LEVEL = logging.DEBUG
 
-logger = logging.getLogger("anicli-api")
-logger.setLevel(LEVEL)
-formatter = logging.Formatter(fmt=FORMAT_LOG, datefmt="%d-%m-%Y %H:%M:%S")
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(formatter)
-console_handler.setLevel(LEVEL)
-logger.addHandler(console_handler)
+
+def init_logger(name: str, level: int = logging.DEBUG):
+    loger = logging.getLogger(name)
+    loger.setLevel(level)
+    formatter = logging.Formatter(fmt=FORMAT_LOG, datefmt="%d-%m-%Y %H:%M:%S")
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(level)
+    loger.addHandler(console_handler)
+
+
+init_logger("anicli-api")
+logger = logging.getLogger("anicli-api.main")
+
+
+if __name__ == '__main__':
+    logger.debug("d")
+    logger.info("i")
+    logger.error("e")
+    logger.warning("w")
+    logger.critical("c")
