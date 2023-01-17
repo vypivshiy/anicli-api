d7aab34ca89f7fe0e2cd20123ae442a301f90d9f
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-01-17 13:13:58 +0300

    black format

--- anicli_api/logging_config.py
+++ anicli_api/logging_config.py
@@ -1,11 +1,11 @@
 import logging
 
-FORMAT_LOG = '%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s'
+FORMAT_LOG = "%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s"
 LEVEL = logging.DEBUG
 
 logger = logging.getLogger("anicli-api")
 logger.setLevel(LEVEL)
-formatter = logging.Formatter(fmt=FORMAT_LOG, datefmt='%d-%m-%Y %H:%M:%S')
+formatter = logging.Formatter(fmt=FORMAT_LOG, datefmt="%d-%m-%Y %H:%M:%S")
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 console_handler.setLevel(LEVEL)
