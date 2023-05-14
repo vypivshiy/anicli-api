809ed76a975717d5edfd29ba2b192ada3c3b62f4
vypivshiy <59173419+vypivshiy@users.noreply.github.com>
2023-05-14 11:40:12 +0300

    set http2 protocol as default

--- anicli_api/_http.py
+++ anicli_api/_http.py
@@ -6,23 +6,23 @@ This module contains httpx.Client and httpx.AsyncClient classes with the followi
 2. x-requested-with: XMLHttpRequest
 
 """
-import logging
 from typing import Dict
-
+import ssl
 from httpx import AsyncClient, Client, Response
 
-logger = logging.getLogger("anicli-api._http")
+from anicli_api._logger import logger
+
+
 HEADERS: Dict[str, str] = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) "
-    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.114"
-    "Mobile Safari/537.36",
+                  "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36",
     # XMLHttpRequest required
     "x-requested-with": "XMLHttpRequest",
 }
 # DDoS protection check by "Server" key header
 DDOS_SERVICES = ("cloudflare", "ddos-guard")
 
-__all__ = ("BaseHTTPSync", "BaseHTTPAsync", "HTTPSync", "HTTPAsync", "Singleton")
+__all__ = ("BaseHTTPSync", "BaseHTTPAsync", "HTTPSync", "HTTPAsync", "Singleton", "ja3_path")
 
 
 class Singleton:
@@ -30,7 +30,7 @@ class Singleton:
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super().__new__(cls, *args, **kwargs)
+            cls._instance = super().__new__(cls)
         return cls._instance
 
 
@@ -38,7 +38,7 @@ class BaseHTTPSync(Client):
     """httpx.Client class with configured user agent and enabled redirects"""
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super().__init__(http2=True, **kwargs)
         self.headers.update(HEADERS)
         self.follow_redirects = True
 
@@ -46,8 +46,8 @@ class BaseHTTPSync(Client):
 class BaseHTTPAsync(AsyncClient):
     """httpx.AsyncClient class with configured user agent and enabled redirects"""
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, http2: bool = True, **kwargs):
+        super().__init__(http2=http2, **kwargs)
         self.headers.update(HEADERS)
         self.follow_redirects = True
 
@@ -59,13 +59,15 @@ def check_ddos_protect_hook(resp: Response):
     If response return 403 code or server headers contains *cloudflare* or *ddos-guard* strings and
     **Connection = close,** throw ConnectionError traceback
     """
+    logger.debug("{} check DDOS protect :\nstatus [{}] {}", resp.url, resp.status_code, resp.headers)
     if (
         resp.headers.get("Server") in DDOS_SERVICES
-        and resp.headers["Connection"] == "close"
+        and resp.headers.get("Connection", None) == "close"
         or resp.status_code == 403
     ):
+        logger.error("Ooops, {} have ddos protect :(", resp.url)
         raise ConnectionError(
-            f"{resp.url} have ddos protect {resp.headers.get('Server')} and return 403 code."
+            f"{resp.url} have '{resp.headers.get('Server', 'unknown')}' and return 403 code."
         )
 
 
