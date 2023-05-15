f98d8fe06db5780bf529b19ea82a226969cf95ca
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-05-15 11:33:02 +0300

    add sovietromantica (black, isort)

--- anicli_api/player/sovetromantica.py
+++ anicli_api/player/sovetromantica.py
@@ -5,7 +5,9 @@ from anicli_api.player.base import BaseVideoExtractor, Video, url_validator
 
 __all__ = ["SovietRomanticaPlayer"]
 # url validator pattern
-_URL_EQ = re.compile(r"https?://(www\.)?[a-z1-9]{1,6}\.sovetromantica\.com/(?:anime|dorama)/.*\.m3u8")
+_URL_EQ = re.compile(
+    r"https?://(www\.)?[a-z1-9]{1,6}\.sovetromantica\.com/(?:anime|dorama)/.*\.m3u8"
+)
 # url validate decorator
 player_validator = url_validator(_URL_EQ)
 
@@ -26,10 +28,10 @@ class SovietRomanticaPlayer(BaseVideoExtractor):
 
     def _extract(self, response) -> List[Video]:
         # any extract logic
-        return [Video(
-            type="m3u8", quality=1080, url=response
-        )]
+        return [Video(type="m3u8", quality=1080, url=response)]
 
 
 if __name__ == "__main__":
-    SovietRomanticaPlayer().parse("https://scp1.sovetromantica.com/anime/1368_akuyaku-reijou-nanode-last-boss-wo-kattemimashita/episodes/subtitles/episode_1/episode_1.m3u8")
+    SovietRomanticaPlayer().parse(
+        "https://scp1.sovetromantica.com/anime/1368_akuyaku-reijou-nanode-last-boss-wo-kattemimashita/episodes/subtitles/episode_1/episode_1.m3u8"
+    )
--- anicli_api/source/sovetromantica.py
+++ anicli_api/source/sovetromantica.py
@@ -5,8 +5,7 @@ from parsel import Selector
 from scrape_schema import ScField
 from scrape_schema.callbacks.parsel import crop_by_xpath_all as cbxa
 from scrape_schema.callbacks.parsel import get_attr, get_text, replace_text
-from scrape_schema.fields.parsel import ParselXPath, NestedParselList
-
+from scrape_schema.fields.parsel import NestedParselList, ParselXPath
 
 from anicli_api.base import (
     BaseAnime,
@@ -14,7 +13,8 @@ from anicli_api.base import (
     BaseExtractor,
     BaseOngoing,
     BaseSearch,
-    BaseSource, MainSchema,
+    BaseSource,
+    MainSchema,
 )
 
 
@@ -24,35 +24,40 @@ class Extractor(BaseExtractor):
     def search(self, query: str) -> List["Search"]:
         response = self.HTTP().get(f"{self.BASE_URL}/anime", params={"query": query})
         return Search.from_crop_rule_list(
-            response.text,
-            crop_rule=cbxa('//div[@class="anime--block__desu"]'))
+            response.text, crop_rule=cbxa('//div[@class="anime--block__desu"]')
+        )
 
     async def a_search(self, query: str) -> List["Search"]:
         # async search entrypoint
-        pass
+        async with self.HTTP_ASYNC() as client:
+            response = await client.get(f"{self.BASE_URL}/anime", params={"query": query})
+            return Search.from_crop_rule_list(response.text,
+                                              crop_rule=cbxa('//div[@class="anime--block__desu"]'))
 
     def ongoing(self) -> List["Ongoing"]:
         # ongoing entrypoint
         response = self.HTTP().get(self.BASE_URL)
         return Ongoing.from_crop_rule_list(
-            response.text,
-            crop_rule=cbxa('//div[@class="anime--block__desu"]')
+            response.text, crop_rule=cbxa('//div[@class="anime--block__desu"]')
         )
 
     async def a_ongoing(self) -> List["Ongoing"]:
         async with self.HTTP_ASYNC() as client:
             response = await client.get(self.BASE_URL)
             return Ongoing.from_crop_rule_list(
-                response.text,
-                crop_rule=cbxa('//div[@class="anime--block__desu"]')
+                response.text, crop_rule=cbxa('//div[@class="anime--block__desu"]')
             )
 
 
 class Search(BaseSearch):
     # past xpath to main anime page
-    url: ScField[str, ParselXPath('//a', callback=get_attr('href'))]
-    name: ScField[str, ParselXPath('//div[@class="anime--block__name"]',
-                                   callback=get_text(deep=True, strip=True, sep=" "))]
+    url: ScField[str, ParselXPath("//a", callback=get_attr("href"))]
+    name: ScField[
+        str,
+        ParselXPath(
+            '//div[@class="anime--block__name"]', callback=get_text(deep=True, strip=True, sep=" ")
+        ),
+    ]
     # url = property(lambda self: f"https://sovetromantica.com{self._path}")
 
     def get_anime(self) -> "Anime":
@@ -70,8 +75,10 @@ class Search(BaseSearch):
 
 class Ongoing(BaseOngoing):
     # past xpath to main anime page
-    _path: ScField[str, ParselXPath('//div[@class="block--full block--shadow"]/a', callback=get_attr('href'))]
-    name: ScField[str, ParselXPath('//meta[@itemprop="name"]', callback=get_attr('content'))]
+    _path: ScField[
+        str, ParselXPath('//div[@class="block--full block--shadow"]/a', callback=get_attr("href"))
+    ]
+    name: ScField[str, ParselXPath('//meta[@itemprop="name"]', callback=get_attr("content"))]
     url = property(lambda self: f"https://sovetromantica.com{self._path}")
 
     def get_anime(self) -> "Anime":
@@ -89,27 +96,32 @@ class Ongoing(BaseOngoing):
 
 class Anime(BaseAnime):
     class _Episode(MainSchema):
-        name: ScField[str, ParselXPath('//a/div/span')]
-        _url_path: ScField[str, ParselXPath('//a', callback=get_attr('href'))]
-        image: ScField[str, ParselXPath('//a/img', callback=get_attr('src'))]
-        ep_id: ScField[str, ParselXPath('//div', callback=get_attr('id'))]
+        name: ScField[str, ParselXPath("//a/div/span")]
+        _url_path: ScField[str, ParselXPath("//a", callback=get_attr("href"))]
+        image: ScField[str, ParselXPath("//a/img", callback=get_attr("src"))]
+        ep_id: ScField[str, ParselXPath("//div", callback=get_attr("id"))]
 
         @property
         def url(self):
             return f"https://sovetromantica.com{self._url_path}"
 
-    name: ScField[str, ParselXPath('//div[@class="block--full anime-name"]/div[@class="block--container"]')]
-    _episodes: ScField[List[_Episode],
-                           NestedParselList(
-                               _Episode,
-                               crop_rule=cbxa("//div[contains(@class, 'episodes-slick_item')]")
-                           )]
+    name: ScField[
+        str, ParselXPath('//div[@class="block--full anime-name"]/div[@class="block--container"]')
+    ]
+    _episodes: ScField[
+        List[_Episode],
+        NestedParselList(
+            _Episode, crop_rule=cbxa("//div[contains(@class, 'episodes-slick_item')]")
+        ),
+    ]
 
     def get_episodes(self) -> List["Episode"]:
         if not self._episodes:
             # episodes may not be uploaded. e.g:
             # https://sovetromantica.com/anime/1398-tsundere-akuyaku-reijou-liselotte-to-jikkyou-no-endou-kun-to-kaisetsu-no-kobayashi-san
-            warnings.warn("Episodes not available, return empty list", category=RuntimeWarning, stacklevel=3)
+            warnings.warn(
+                "Episodes not available, return empty list", category=RuntimeWarning, stacklevel=3
+            )
             return []
         return [Episode.from_kwargs(**ep.dict()) for ep in self._episodes]
 
@@ -117,7 +129,9 @@ class Anime(BaseAnime):
         if not self._episodes:
             # episodes may not be uploaded. e.g:
             # https://sovetromantica.com/anime/1398-tsundere-akuyaku-reijou-liselotte-to-jikkyou-no-endou-kun-to-kaisetsu-no-kobayashi-san
-            warnings.warn("Episodes not available, return empty list", category=RuntimeWarning, stacklevel=3)
+            warnings.warn(
+                "Episodes not available, return empty list", category=RuntimeWarning, stacklevel=3
+            )
             return []
         return [Episode.from_kwargs(**ep.dict()) for ep in self._episodes]
 
@@ -135,8 +149,8 @@ class Episode(BaseEpisode):
         response = self.HTTP().get(self.url)
         sel = Selector(response.text)
         video = ParselXPath(
-            '//meta[@property="ya:ovs:content_url"]',  callback=get_attr('content')
-            ).extract(sel)
+            '//meta[@property="ya:ovs:content_url"]', callback=get_attr("content")
+        ).extract(sel)
         return [Source.from_kwargs(url=video)]
 
     async def a_get_sources(self) -> List["Source"]:
@@ -144,7 +158,7 @@ class Episode(BaseEpisode):
             response = await client.get(self.url)
             sel = Selector(response.text)
             video = ParselXPath(
-                '//meta[@property="ya:ovs:content_url"]', callback=get_attr('content')
+                '//meta[@property="ya:ovs:content_url"]', callback=get_attr("content")
             ).extract(sel)
             return [Source.from_kwargs(url=video)]
 
@@ -160,9 +174,9 @@ class Source(BaseSource):
         return self.name
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ex = Extractor()
-    s = ex.search('lai')
+    s = ex.search("lai")
     ong = ex.ongoing()
     an = s[1].get_anime()
     s[0].get_anime().get_episodes()  # not founded eps
