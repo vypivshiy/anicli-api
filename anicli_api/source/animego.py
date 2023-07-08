0824b90f5fe20d4070a79a5022563a1c7a10924c
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-07-08 12:17:05 +0300

    update selectors

--- anicli_api/source/animego.py
+++ anicli_api/source/animego.py
@@ -1,6 +1,7 @@
-from typing import Dict, List
+from typing import Dict, List, Tuple
 from urllib.parse import urlsplit
 
+from parsel import Selector
 from scrape_schema import Sc, Parsel, sc_param
 
 from anicli_api.base import (
@@ -31,16 +32,28 @@ class Extractor(BaseExtractor):
             ).getall().sc_parse(response.text)
             return [Search(chunk) for chunk in chunks]
 
+    @staticmethod
+    def _ongoing_clear_dupes(ongoings: List["Ongoing"]) -> List["Ongoing"]:
+        """remove url duplicates for decrease output result"""
+        result: Dict[Tuple[int, str], Ongoing] = {}
+        for ongoing in ongoings:
+            tuple_key = (ongoing.num, ongoing.url)
+            if not result.get(tuple_key, None):
+                result[tuple_key] = ongoing
+            else:
+                result[tuple_key].dub += f", {ongoing.dub}"
+        return list(result.values())
+
     def ongoing(self) -> List["Ongoing"]:
         response = self.HTTP().get(self.BASE_URL)
         chunks = Parsel().xpath('//*[starts-with(@class, "last-update-item")]').getall().sc_parse(response.text)
-        return [Ongoing(chunk) for chunk in chunks]
+        return self._ongoing_clear_dupes([Ongoing(chunk) for chunk in chunks])
 
     async def a_ongoing(self) -> List["Ongoing"]:
         async with self.HTTP_ASYNC() as client:
             response = await client.get(self.BASE_URL)
             chunks = Parsel().xpath('//*[starts-with(@class, "last-update-item")]').getall().sc_parse(response.text)
-            return [Ongoing(chunk) for chunk in chunks]
+            return self._ongoing_clear_dupes([Ongoing(chunk) for chunk in chunks])
 
 
 class Search(BaseSearch):
@@ -66,7 +79,7 @@ class Search(BaseSearch):
 class Ongoing(BaseOngoing):
     _thumb_style: Sc[str, Parsel().xpath("//div[@class='img-square lazy br-50']/@style").get()]
     title: Sc[str, Parsel().xpath("//span[@class='last-update-title font-weight-600']/text()").get()]
-    episode: Sc[str, Parsel().get("//div[@class='font-weight-600 text-truncate']/text()").get()]
+    episode: Sc[str, Parsel().xpath("//div[@class='font-weight-600 text-truncate']/text()").get()]
     dub: Sc[str, Parsel().xpath("//div[@class='ml-3 text-right']/div[@class='text-gray-dark-6']/text()").get()]
     _onclick: Sc[str, Parsel().xpath("//div/@onclick").get()]
 
@@ -97,16 +110,42 @@ class Ongoing(BaseOngoing):
 
 
 class Anime(BaseAnime):
-    rating: Sc[float, Parsel(default=.0).xpath(
-        '//*[@id="itemRatingBlock"]/div[1]/div[2]/div[1]/span[1]/text()').get().sc_replace(",", ".")]
-    title: Sc[str, Parsel().xpath("//div[@class='anime-title']/div/h1/text()").get()]
-    alt_titles: Sc[List[str], Parsel().xpath('//ul[@class="list-unstyled small mb-0"]/li/text()').getall()]
-    # todo create normal structure
-    _raw_metadata: Sc[str, Parsel().xpath('//div[@class="anime-info"]/dl[@class="row"]/text()').getall()]
+    # todo write
+    _script_jmespath: Sc[Selector, Parsel(auto_type=False).xpath("//script[@type='application/ld+json']/text()")]
+    # rating: Sc[float, Parsel(default=.0).xpath(
+    #     '//*[@id="itemRatingBlock"]/div[1]/div[2]/div[1]/span[1]/text()').get().sc_replace(",", ".")]
+    # title: Sc[str, Parsel().xpath("//div[@class='anime-title']/div/h1/text()").get()]
+    # alt_titles: Sc[List[str], Parsel().xpath('//ul[@class="list-unstyled small mb-0"]/li/text()').getall()]
     url: Sc[str, Parsel().xpath("//html/head/link[@rel='canonical']/@href").get()]
-    description: Sc[str, Parsel().xpath("//div[@class='description pb-3']/text()").get()]  # mobile agent
-    # ParselXPath("//div[@data-readmore='content']", callback=get_text(strip=True, sep=" "))]  # desktop agent
+    # "//div[@class='description pb-3']/text()"
+    _description: Sc[List[str], Parsel().xpath("//div[@data-readmore='content']/text()").getall()]  # mobile agent
     anime_id = sc_param(lambda self: self.url.split("-")[-1])
+
+    @sc_param
+    def description(self) -> str:
+        return " ".join(s.strip() for s in self._description)
+
+    @sc_param
+    def release(self) -> str:
+        if end_date := self._script_jmespath.jmespath("endDate").get():
+            return self._script_jmespath.jmespath("startDate").get() + " " + end_date
+        return self._script_jmespath.jmespath("startDate").get()
+
+    @sc_param
+    def genre(self) -> List[str]:
+        return self._script_jmespath.jmespath("genre").getall()
+
+    @sc_param
+    def rating(self) -> float:
+        return float(self._script_jmespath.jmespath("aggregateRating.ratingValue").get())
+
+    @sc_param
+    def title(self) -> str:
+        return self._script_jmespath.jmespath("name").get()
+
+    @sc_param
+    def alt_titles(self) -> List[str]:
+        return self._script_jmespath.jmespath("alternativeHeadline").getall()
 
     def __str__(self):
         return f"{self.title} {self.alt_titles} ({self.rating})"
@@ -199,9 +238,13 @@ class Source(BaseSource):
 
 if __name__ == "__main__":
     ex = Extractor()
+    # res = ex.ongoing()
     res = ex.search("lain")
     an = res[0].get_anime()
-    episodes = an.get_episodes()
-    sss = episodes[0].get_sources()
-    vids = sss[0].get_videos()
-    print(*vids)
+    ongs = ex.ongoing()
+    an2 = ongs[0].get_anime()
+    print()
+    # episodes = an.get_episodes()
+    # sss = episodes[0].get_sources()
+    # vids = sss[0].get_videos()
+    # print(*vids)
