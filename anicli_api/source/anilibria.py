7d267d2bc5ee54b031b338f8ef5b49d4ef4d60e0
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-10-01 00:32:11 +0300

    standardize to project API

--- anicli_api/source/anilibria.py
+++ anicli_api/source/anilibria.py
@@ -56,71 +56,80 @@ class Extractor(BaseExtractor):
     BASE_URL = "https://api.anilibria.tv/v2/"  # BASEURL
     API = Anilibria()
 
+    @staticmethod
+    def __extract_meta_data(kw: dict) -> dict:
+        """extract response data for anicli application"""
+        return dict(
+            title=kw['names']['ru'],
+            url='STUB VALUE',
+            thumbnail=f"https://{kw['player']['host']}{kw['posters']['small']['url']}",
+            # anime_info meta
+            _alt_titles=[kw['names'][n] for n in kw['names'].keys() if kw['names'][n] is not None and n != 'ru'],
+            _description=kw['description'],
+            _genres=kw['genres'],
+            _episodes_available=kw['torrents']['series']['last'],
+            _episodes_total=kw['type']['series'],
+            _aired=kw['season']['year'],  # TODO format to full date
+            # episodes and video meta
+            _episodes_and_videos=kw['player']['playlist'],
+            _host=kw['player']['host']
+        )
+
     def search(self, query: str) -> List["Search"]:
         # search entrypoint
-        return [Search.from_kwargs(**kw) for kw in self.API.search_titles(search=query)]
+        return [
+            Search.from_kwargs(**self.__extract_meta_data(kw))
+            for kw in self.API.search_titles(search=query)]
 
     async def a_search(self, query: str) -> List["Search"]:
         # async search entrypoint
-        return [Search.from_kwargs(**kw) for kw in (await self.API.a_search_titles(search=query))]
+        return [Search.from_kwargs(**self.__extract_meta_data(kw))
+                for kw in (await self.API.a_search_titles(search=query))]
 
     def ongoing(self) -> List["Ongoing"]:
         # ongoing entrypoint
-        return [Ongoing.from_kwargs(**kw) for kw in self.API.get_updates()]
+        return [Search.from_kwargs(**self.__extract_meta_data(kw))
+                for kw in self.API.get_updates()]
 
     async def a_ongoing(self) -> List["Ongoing"]:
         # async ongoing entrypoint
-        return [Ongoing.from_kwargs(**kw) for kw in (await self.API.a_get_updates())]
+        return [Search.from_kwargs(**self.__extract_meta_data(kw))
+                for kw in (await self.API.a_get_updates())]
 
 
 class _SearchOrOngoing(MainSchema):
-    id: int
-    code: str
-    names: dict
-    announce: Optional[Any]
-    status: dict
-    posters: dict
-    updated: int
-    last_change: int
-    type: dict
-    genres: List[str]
-    team: dict
-    season: dict
-    description: str
-    in_favorites: int
-    blocked: dict
-    player: dict
-    torrents: dict
-
-    def dict(self) -> Dict[str, Any]:
-        return {
-            "id": self.id,
-            "code": self.code,
-            "names": self.names,
-            "announce": self.announce,
-            "status": self.status,
-            "posters": self.posters,
-            "updated": self.updated,
-            "last_change": self.last_change,
-            "type": self.type,
-            "genres": self.genres,
-            "team": self.team,
-            "season": self.season,
-            "description": self.description,
-            "in_favorites": self.in_favorites,
-            "blocked": self.blocked,
-            "player": self.player,
-            "torrents": self.torrents,
-        }
+    url: str
+    title: str
+    thumbnail: str
+    # AnimeInfo meta
+    _alt_titles: list[str]
+    _description: str
+    _genres: list[str]
+    _episodes_available: int
+    _episodes_total: int
+    _aired: str
+    # Episode and Video meta
+    _episodes_and_videos: dict
+    _host: str
 
     async def a_get_anime(self) -> "Anime":
         return self.get_anime()
 
     def get_anime(self) -> "Anime":
-        return Anime.from_kwargs(**self.__dict__)  # dict() method didn't see annotated attrs
+        return Anime.from_kwargs(
+            title=self.title,
+            alt_title=self._alt_titles,
+            description=self._description,
+            genres=self._genres,
+            episodes_available=self._episodes_available,
+            episodes_total=self._episodes_total,
+            aired=self._aired,
+            _episodes_and_videos=self._episodes_and_videos,
+            _host=self._host
+        )
 
     def __str__(self):
-        return f"{list(self.names.values())[0]}"
+        return self.title
 
 
 class Search(_SearchOrOngoing, BaseSearch):
@@ -132,57 +141,27 @@ class Ongoing(_SearchOrOngoing, BaseOngoing):
 
 
 class Anime(BaseAnime):
-    id: int
-    code: str
-    names: dict
-    announce: Optional[Any]
-    status: dict
-    posters: dict
-    updated: int
-    last_change: int
-    type: dict
-    genres: List[str]
-    team: dict
-    season: dict
+    title: str
+    alt_title: str
     description: str
-    in_favorites: int
-    blocked: dict
-    player: dict
-    torrents: dict
-
-    def dict(self) -> Dict[str, Any]:
-        return {
-            "id": self.id,
-            "code": self.code,
-            "names": self.names,
-            "announce": self.announce,
-            "status": self.status,
-            "posters": self.posters,
-            "updated": self.updated,
-            "last_change": self.last_change,
-            "type": self.type,
-            "genres": self.genres,
-            "team": self.team,
-            "season": self.season,
-            "description": self.description,
-            "in_favorites": self.in_favorites,
-            "blocked": self.blocked,
-            "player": self.player,
-            "torrents": self.torrents,
-        }
+    genres: list[str]
+    episodes_available: int
+    episodes_total: int
+    aired: int
+    _episodes_and_videos: dict
+    _host: str
 
     def __str__(self):
-        return f"{list(self.names.values())}"
+        return self.title
 
     def get_episodes(self) -> List["Episode"]:
         return [
-            Episode.from_kwargs(
-                alternative_player=self.player["alternative_player"],
-                host=self.player["host"],
-                torrents=self.torrents["list"],
-                **p,
-            )
-            for p in self.player["playlist"].values()
+            Episode.from_kwargs(title=f'Episode {num}',
+                                num=item['serie'],
+                                _fhd=f"https://{self._host}{item['hls']['fhd']}",
+                                _hd=f"https://{self._host}{item['hls']['hd']}",
+                                _sd=f"https://{self._host}{item['hls']['sd']}")
+            for num, item in self._episodes_and_videos.items()
         ]
 
     async def a_get_episodes(self) -> List["Episode"]:
@@ -190,37 +169,24 @@ class Anime(BaseAnime):
 
 
 class Episode(BaseEpisode):
-    alternative_player: Optional[str]
-    host: str
-    serie: int
-    created_timestamp: int
-    preview: Optional[Any]
-    skips: dict
-    hls: dict
-    torrents: dict
-
-    def dict(self) -> Dict[str, Any]:
-        return {
-            "alternative_player": self.alternative_player,
-            "host": self.host,
-            "serie": self.serie,
-            "created_timestamp": self.created_timestamp,
-            "preview": self.preview,
-            "skips": self.skips,
-            "hls": self.hls,
-            "torrents": self.torrents,
-        }
+    title: str
+    num: int
+    # video meta
+    _fhd: str
+    _hd: str
+    _sd: str
 
     def __str__(self):
-        return f"{self.host} {self.serie}"
+        return self.title
 
     def get_sources(self) -> List["Source"]:
         return [
             Source.from_kwargs(
-                torrents=self.torrents,
-                # dirty hack for success url validate for decoder.anilibria :D
-                url=self.hls["sd"],
-                **{k: f"https://{self.host}{v}" if v else None for k, v in self.hls.items()},
+                name="Anilibria",
+                url="",
+                _fhd=self._fhd,
+                _hd=self._hd,
+                _sd=self._sd
             )
         ]
 
@@ -229,27 +195,25 @@ class Episode(BaseEpisode):
 
 
 class Source(BaseSource):
-    torrents: dict
-    fhd: Optional[str]
-    hd: str
-    sd: str
-
-    def dict(self) -> Dict[str, Any]:
-        return {"torrents": self.torrents, "fhd": self.fhd, "hd": self.hd, "sd": self.sd}
+    url: str
+    name: str
+    _fhd: str
+    _hd: str
+    _sd: str
 
     def __str__(self):
-        return f"{urlsplit(self.fhd).netloc or urlsplit(self.hd).netloc}"
+        return f"{urlsplit(self._sd).netloc} ({self.name})"
 
     def get_videos(self) -> List["Video"]:
-        if self.fhd:
+        if self._fhd:
             return [
-                Video(type="m3u8", quality=480, url=self.sd),
-                Video(type="m3u8", quality=720, url=self.hd),
-                Video(type="m3u8", quality=1080, url=self.fhd),
+                Video(type="m3u8", quality=480, url=self._sd),
+                Video(type="m3u8", quality=720, url=self._hd),
+                Video(type="m3u8", quality=1080, url=self._fhd),
             ]
         return [
-            Video(type="m3u8", quality=480, url=self.sd),
-            Video(type="m3u8", quality=720, url=self.hd),
+            Video(type="m3u8", quality=480, url=self._sd),
+            Video(type="m3u8", quality=720, url=self._hd),
         ]
 
     async def a_get_videos(self) -> List["Video"]:
@@ -258,7 +222,8 @@ class Source(BaseSource):
 
 if __name__ == "__main__":
     ex = Extractor()
-    r = ex.search("lai")
+    r = ex.search("магическая битва")
+    print(r[0].dict())
     an = r[0].get_anime()
     eps = an.get_episodes()
     sss = eps[0].get_sources()
