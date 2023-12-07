08754bf64d30ed4580edc7772408e8075bc9b61b
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-12-07 16:32:31 +0300

    drop colorlog, scrape-schema loggers config

--- anicli_api/_logger.py
+++ anicli_api/_logger.py
@@ -1,20 +1,12 @@
 import logging
 
-import colorlog
 
 __all__ = ["logger"]
 
 
-handler = colorlog.StreamHandler()
-_formatter = colorlog.ColoredFormatter(fmt="%(log_color)s %(asctime)s [%(levelname)-8s] %(name)s: %(message)s'")
+handler = logging.StreamHandler()
+_formatter = logging.Formatter(fmt="%(log_color)s %(asctime)s [%(levelname)-8s] %(name)s: %(message)s'")
 
 logger = logging.getLogger("anicli-api")  # type: ignore
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
-
-
-_logger_cast = logging.getLogger("type_caster")
-_logger_cast.setLevel(logging.ERROR)  # type: ignore
-
-_sc_schema_logger = logging.getLogger("scrape_schema")
-_sc_schema_logger.setLevel(logging.ERROR)
