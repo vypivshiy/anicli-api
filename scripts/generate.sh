908603a3851ee04db55bf25c0a0795487a5d8f03
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-12-07 20:27:03 +0300

    drop example, update generate script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,15 +1,30 @@
+#!/bin/bash
+
 repo_dir="libanime_schema"
 github_repo="https://github.com/libanime/libanime_schema"
 
-# TODO check updates
-if [ -d "$repo_dir" ]; then
-    echo "Directory $repo_dir already exists."
-else
-    echo "Directory $repo_dir does not exist. Cloning the repository..."
+clone_repository() {
+    echo "Cloning the repository..."
     git clone "$github_repo" "$repo_dir"
-fi
+}
 
-ssc-gen libanime_schema/source/animego.yaml python.parsel -o anicli_api/source/parsers/animego_parser.py -y
-ssc-gen libanime_schema/source/animania.yaml python.parsel -o anicli_api/source/parsers/animania_parser.py -y
-ssc-gen libanime_schema/source/animejoy.yaml python.parsel -o anicli_api/source/parsers/animejoy_parser.py -y
-ssc-gen libanime_schema/source/sovetromantica.yaml python.parsel -o anicli_api/source/parsers/sovetromantica_parser.py -y
+check_and_clone_repository() {
+    if [ -d "$repo_dir" ]; then
+        echo "Directory $repo_dir already exists, generate"
+    else
+        clone_repository
+    fi
+}
+
+generate_parser() {
+    ssc-gen "$1" "$2" -o "$3" -y
+}
+
+# Check and clone the repository
+check_and_clone_repository
+
+# Generate parsers
+generate_parser "libanime_schema/source/animego.yaml" "python.parsel" "anicli_api/source/parsers/animego_parser.py"
+generate_parser "libanime_schema/source/animania.yaml" "python.parsel" "anicli_api/source/parsers/animania_parser.py"
+generate_parser "libanime_schema/source/animejoy.yaml" "python.parsel" "anicli_api/source/parsers/animejoy_parser.py"
+generate_parser "libanime_schema/source/sovetromantica.yaml" "python.parsel" "anicli_api/source/parsers/sovetromantica_parser.py"
