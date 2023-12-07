5ba35183d7455e6655602f5900a37e0fa9882ab0
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-12-07 14:25:52 +0300

    add git fetch

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,3 +1,14 @@
+repo_dir="libanime_schema"
+github_repo="https://github.com/libanime/libanime_schema"
+
+# TODO check updates
+if [ -d "$repo_dir" ]; then
+    echo "Directory $repo_dir already exists."
+else
+    echo "Directory $repo_dir does not exist. Cloning the repository..."
+    git clone "$github_repo" "$repo_dir"
+fi
+
 ssc-gen libanime_schema/source/animego.yaml python.parsel -o anicli_api/source/parsers/animego_parser.py -y
 ssc-gen libanime_schema/source/animania.yaml python.parsel -o anicli_api/source/parsers/animania_parser.py -y
 ssc-gen libanime_schema/source/animejoy.yaml python.parsel -o anicli_api/source/parsers/animejoy_parser.py -y
