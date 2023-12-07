5c5a3f6e7622fbd7e7371341a462873763825bdc
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-12-07 10:25:11 +0300

    add codegen schemas script

---
+++ scripts/generate.sh
@@ -0,0 +1,4 @@
+ssc-gen libanime_schema/source/animego.yaml python.parsel -o anicli_api/source/parsers/animego_parser.py -y
+ssc-gen libanime_schema/source/animania.yaml python.parsel -o anicli_api/source/parsers/animania_parser.py -y
+ssc-gen libanime_schema/source/animejoy.yaml python.parsel -o anicli_api/source/parsers/animejoy_parser.py -y
+ssc-gen libanime_schema/source/sovetromantica.yaml python.parsel -o anicli_api/source/parsers/sovetromantica_parser.py -y
