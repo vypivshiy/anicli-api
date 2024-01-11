7d67c74f4b73897314748cc6e141a8740cce6df4
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-01-11 22:00:46 +0300

    sibnet referer fix

--- anicli_api/player/sibnet.py
+++ anicli_api/player/sibnet.py
@@ -14,20 +14,20 @@ class SibNet(BaseVideoExtractor):
     @player_validator
     def parse(self, url: str, **kwargs) -> List[Video]:
         response = self.http.get(url).text
-        return self._extract(response)
+        return self._extract(response, referer=url)
 
     @player_validator
     async def a_parse(self, url: str, **kwargs) -> List[Video]:
         async with self.a_http as client:
             response = (await client.get(url)).text
-            return self._extract(response)
+            return self._extract(response, referer=url)
 
-    def _extract(self, response: str) -> List[Video]:
+    def _extract(self, response: str, referer: str) -> List[Video]:
         if path := re.search(r'"(?P<url>/v/.*?\.mp4)"', response):
             url = f"https://video.sibnet.ru{path[1]}"
-            return [Video(type="mp4", quality=480, url=url, headers={"Referer": url})]
+            return [Video(type="mp4", quality=480, url=url, headers={"Referer": referer})]
         return []
 
 
 if __name__ == "__main__":
-    print(SibNet().parse()[0].url)
+    print(SibNet().parse()[0])
