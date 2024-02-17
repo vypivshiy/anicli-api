7ee8b5191e83cbad728e1f268505376dc39cfac9
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-02-17 11:23:31 +0300

    add nuum tests, add test_not_found_video_url

--- tests/test_players.py
+++ tests/test_players.py
@@ -14,6 +14,7 @@ from anicli_api.player import (
     SibNet,
     SovietRomanticaPlayer,
     VkCom,
+    Nuum
 )
 from anicli_api.player.base import BaseVideoExtractor
 
@@ -27,27 +28,29 @@ from anicli_api.player.base import BaseVideoExtractor
         (Kodik, "https://kodik.info/seria/1133512/04d5f7824ba3563bd78e44a22451bb45/720p", 3, ("m3u8",), 0),
         (CsstOnline, "https://csst.online/embed/487794", 4, ("mp4",), 0),
         (
-            Dzen,
-            "https://dzen.ru/embed/vh1fMeui3d3Y?from_block=partner&from=zen&mute=1&autoplay=0&tv=0",
-            3,
-            ("mpd", "m3u8", "audio"),
-            -1,
+                Dzen,
+                "https://dzen.ru/embed/vh1fMeui3d3Y?from_block=partner&from=zen&mute=1&autoplay=0&tv=0",
+                3,
+                ("mpd", "m3u8", "audio"),
+                -1,
         ),
         (MailRu, "https://my.mail.ru/video/embed/358279802395848306", 2, ("mp4",), 0),
         (OkRu, "https://ok.ru/videoembed/4998442453635", 6, ("mp4",), 0),
         (SibNet, "https://video.sibnet.ru/shell.php?videoid=4779967", 1, ("mp4",), 0),
         (
-            SovietRomanticaPlayer,
-            "https://scu3.sovetromantica.com/anime/1253_100-man-no-inochi-no-ue-ni-ore-wa-tatteiru-2nd-season/episodes/subtitles/episode_1/episode_1.m3u8",
-            1,
-            ("m3u8",),
-            0,
+                SovietRomanticaPlayer,
+                "https://scu3.sovetromantica.com/anime/1253_100-man-no-inochi-no-ue-ni-ore-wa-tatteiru-2nd-season/episodes/subtitles/episode_1/episode_1.m3u8",
+                1,
+                ("m3u8",),
+                0,
         ),
         (VkCom, "https://vk.com/video_ext.php?oid=793268683&id=456239019&hash=0f28589bfca114f7", 5, ("mp4",), 0),
+        (Nuum, 'https://nuum.ru/embed/record/1610248', 1, ('m3u8',), 0),
+
     ],
 )
 def test_sync_video_extractor(
-    player: Type[BaseVideoExtractor], url: str, count: int, types: Tuple[str, ...], index: int
+        player: Type[BaseVideoExtractor], url: str, count: int, types: Tuple[str, ...], index: int
 ):
     if (status := player().http.get(url).status_code) and status != 200:
         pytest.skip(f"Player {player.__name__} return [{status}] code.")
@@ -65,12 +68,21 @@ def test_sync_video_extractor(
     )
 
 
+@pytest.mark.parametrize('extractor, url', (
+        (Nuum(), 'https://nuum.ru/embed/record/1549072'),
+        (Kodik(), 'https://kodik.info/seria/310427/09985563d891b56b1e9b01142ae11872/720p')
+))
+def test_not_found_video_url(extractor, url):
+    assert len(extractor.parse(url)) == 0
+
+
 def test_animejoy():
     # This video extractor is only relevant for ONGOINGS
-    # (After completed, videos are deleted from the servers).
+    # (After release, videos are deleted from the servers).
     # Written this TEST CASE to avoid rewriting relevant source
     videos = AnimeJoy().parse(
-        "https://animejoy.ru/player/playerjs.html?file=[1080p]https://noda3.cdnjoy.site/Tsunlise/KAZOKU/01-1080.mp4,[360p]https://noda3.cdnjoy.site/Tsunlise/KAZOKU/01-360.mp4"
+        "https://animejoy.ru/player/playerjs.html?file=[1080p]https://noda3.cdnjoy.site/Tsunlise/KAZOKU/01-1080.mp4,"
+        "[360p]https://noda3.cdnjoy.site/Tsunlise/KAZOKU/01-360.mp4"
     )
 
     assert len(videos) == 2
