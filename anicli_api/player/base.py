2f0fb5a6d10b89876828b8886254c7937e43546b
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-02-17 11:22:38 +0300

    typing url_validator decorator, change video dataclass to attrs
    refactoring ABCVideoExtractor

--- anicli_api/player/base.py
+++ anicli_api/player/base.py
@@ -1,39 +1,44 @@
 import re
 from abc import ABC, abstractmethod
-from dataclasses import dataclass, field
 from functools import wraps
-from typing import Any, Dict, List, Literal, Union
+from typing import Any, Dict, List, Literal, Union, TypeVar, Callable
 from urllib.parse import urlparse
+
+from attrs import define, Factory
 
 from anicli_api._http import BaseHTTPAsync, BaseHTTPSync
 
+__all__ = ["ALL_QUALITIES", "Video", "url_validator", "BaseVideoExtractor", "ABCVideoExtractor"]
+
 ALL_QUALITIES = (144, 240, 360, 480, 720, 1080)
+T = TypeVar("T")
 
 
-def url_validator(pattern: Union[str, re.Pattern]):
+def url_validator(pattern: Union[str, re.Pattern]) -> Callable[..., Callable[..., T]]:
     """check valid url for extractor"""
     if isinstance(pattern, str):
         pattern = re.compile(pattern)
 
     def decorator(func):
         @wraps(func)
-        def wrapper(_, url, **kwargs):
+        def wrapper(cls, url, **kwargs):
             if not pattern.match(url):
-                raise TypeError(f"Uncorrected url for {_.__class__.__name__} player")
-            return func(_, url, **kwargs)
+                msg = f"Uncorrected url for {cls.__class__.__name__} player"
+                raise TypeError(msg)
+            return func(cls, url, **kwargs)
 
         return wrapper
 
     return decorator
 
 
-@dataclass
+@define(eq=False)
 class Video:
     """Video container contains direct link and information like type, quality
 
-    - type - video format type (mp4, m3u8 or mpd)
+    - type - video format type ["mp4", "m3u8", "mpd", "audio", "webm"]
 
-    - quality - video quality [144, 240, 360, 480, 720, 1080]
+    - quality - video quality [0, 144, 240, 360, 480, 720, 1080] 0 - audio
 
     - url - direct video link
 
@@ -43,7 +48,7 @@ class Video:
     type: Literal["mp4", "m3u8", "mpd", "audio", "webm"]
     quality: Literal[0, 144, 240, 360, 480, 720, 1080]
     url: str
-    headers: Dict[str, str] = field(default_factory=dict)
+    headers: Dict[str, str] = Factory(dict)
 
     def dict(self) -> Dict[str, Any]:
         return {
@@ -62,7 +67,8 @@ class Video:
     def __eq__(self, other):
         if isinstance(other, Video):
             return hash(self) == hash(other)
-        raise TypeError(f"Video object required, not {type(other)}")
+        msg = f"Video object required, not {type(other)}"
+        raise TypeError(msg)
 
 
 class ABCVideoExtractor(ABC):
@@ -75,9 +81,10 @@ class ABCVideoExtractor(ABC):
         """
         :type httpx_kwargs: httpx.Client and httpx.AsyncClient configuration
         """
-        default_kwargs = {k: v for k, v in self.DEFAULT_HTTP_CONFIG.items() if not httpx_kwargs.get(k)}
-        self.http = BaseHTTPSync(**default_kwargs, **httpx_kwargs)
-        self.a_http = BaseHTTPAsync(**default_kwargs, **httpx_kwargs)
+        default_kwargs = self.DEFAULT_HTTP_CONFIG.copy()
+        default_kwargs.update(httpx_kwargs)
+        self.http = BaseHTTPSync(**default_kwargs)
+        self.a_http = BaseHTTPAsync(**default_kwargs)
 
     @abstractmethod
     def parse(self, url: str, **kwargs) -> List[Video]:
@@ -95,7 +102,8 @@ class ABCVideoExtractor(ABC):
     def __eq__(self, other: str):  # type: ignore
         """compare class instance with url string"""
         if not isinstance(other, str):
-            raise TypeError(f"{other} should be str not {type(other).__name__}")
+            msg = f"{other} should be str not {type(other).__name__}"
+            raise TypeError(msg)
         return self._compare_url(other)
 
 
