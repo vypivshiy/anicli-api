883460cdee9345e1f5e4e16f2ec4cbfc8b97ec17
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-08-04 11:51:39 +0300

    drop unused code

--- anicli_api/player/__init__.py
+++ anicli_api/player/__init__.py
@@ -1,5 +1,4 @@
 from anicli_api.player.aniboom import Aniboom
-from anicli_api.player.animejoy import AnimeJoy
 from anicli_api.player.csst import CsstOnline
 from anicli_api.player.dzen import Dzen
 from anicli_api.player.kodik import Kodik
@@ -10,4 +9,4 @@ from anicli_api.player.sibnet import SibNet
 from anicli_api.player.sovetromantica import SovietRomanticaPlayer
 from anicli_api.player.vkcom import VkCom
 
-ALL_DECODERS = (Kodik, Aniboom, SibNet, AnimeJoy, CsstOnline, MailRu, OkRu, VkCom, Dzen, SovietRomanticaPlayer, Nuum)
+ALL_DECODERS = (Kodik, Aniboom, SibNet, CsstOnline, MailRu, OkRu, VkCom, Dzen, SovietRomanticaPlayer, Nuum)
