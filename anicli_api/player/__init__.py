2f9ad72b84cb79074c964903af987037c90c73d8
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-09-29 19:50:18 +0300

    fix players imports, bump

--- anicli_api/player/__init__.py
+++ anicli_api/player/__init__.py
@@ -1,12 +1,7 @@
 from anicli_api.player.aniboom import Aniboom
 from anicli_api.player.csst import CsstOnline
-from anicli_api.player.dzen import Dzen
 from anicli_api.player.kodik import Kodik
-from anicli_api.player.mailru import MailRu
-from anicli_api.player.nuum import Nuum
-from anicli_api.player.okru import OkRu
 from anicli_api.player.sibnet import SibNet
 from anicli_api.player.sovetromantica import SovietRomanticaPlayer
-from anicli_api.player.vkcom import VkCom
 
-ALL_DECODERS = (Kodik, Aniboom, SibNet, CsstOnline, MailRu, OkRu, VkCom, Dzen, SovietRomanticaPlayer, Nuum)
+ALL_DECODERS = (Kodik, Aniboom, SibNet, CsstOnline, SovietRomanticaPlayer)
