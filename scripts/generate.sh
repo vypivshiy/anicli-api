552ce9e8f127d8524d4bb7e85993a52b331809a6
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2026-08-05 18:58:24 +0300

    update generate scripts

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
-ssc-gen generate -R -t py-lxml -o "anicli_api/player/parsers" --http-client httpx dev/player
-ssc-gen generate -R -t py-lxml -o "anicli_api/source/parsers" --http-client httpx dev/src 
+# ssc-gen generate -R -t py-lxml -o "anicli_api/player/parsers" --http-client httpx dev/player
+# ssc-gen generate -R -t py-lxml -o "anicli_api/source/parsers" --http-client httpx dev/src 
+ssc-gen generate python .\dev\player -L lxml -R -o .\anicli_api\player\parsers
+ssc-gen generate python .\dev\src -L lxml -R -o .\anicli_api\source\parsers
